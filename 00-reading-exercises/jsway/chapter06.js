549c2dc20159a9e8bb896c014f46d74e76c597d5
BitLayZzz <blackburton23@gmail.com>
2022-03-28 13:08:47 -0700

    Finished reading for week 11; Started working on JS Ch. 6 reading exercises;made repository for upcoming calculator assignment

---
+++ 00-reading-exercises/jsway/chapter06.js
@@ -0,0 +1,69 @@
+// Coding challenges from chapter 06 Create your first objects
+
+// 1
+// Adding character experience
+//
+// Improve our example RPG program to add an experience property named xp 
+// to the character. Its initial value is 0. Experience must appear in 
+// character description.
+
+
+// TODO: create the character object here
+
+// const aurora = {
+//   name: "Aurora",
+//   health: 150,
+//   strength: 25,
+//   xp: 0,
+//
+//   // Return the character description
+//   describe() {
+//     return `${this.name} has ${this.health} health points and ${this
+//       .strength} as strength and ${this.xp} XP points`;
+//   }
+// }
+//
+// // Aurora is harmed by an arrow
+// aurora.health -= 20;
+//
+// // Aurora equips a strength necklace
+// aurora.strength += 10;
+//
+// // Aurora learn a new skill
+// aurora.xp += 15;
+//
+// console.log(aurora.describe());
+//
+
+// 2
+// Complete the following program to add the dog object definition.
+// TODO: create the dog object here
+// Constructor function
+
+// function dog(name, species, size){
+//   this.name = name;
+//   this.species = species;
+//   this.size = size;
+// }
+//
+// dog.bark = _ => "Grrr! Grrr!"; //Method creation
+//
+// let fang = new dog();
+// fang.name = 'Fang';
+// fang.species = 'boarhound';
+// fang.size = 75;
+//
+// console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
+// console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`);
+
+
+// 3 
+// Complete the following program to add the circle object definition. 
+// Its radius value is input by the user.
+// const r = Number(prompt("Enter the circle radius:"));
+//
+// // TODO: create the circle object here
+//
+// console.log(`Its circumference is ${circle.circumference()}`);
+// console.log(`Its area is ${circle.area()}`);
+
