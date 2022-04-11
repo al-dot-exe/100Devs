64d01a49ee1a04187b24131d008b3cc845cfbd4e
Al.exe <alEXE-tech@protonmail.com>
2022-04-11 11:55:40 -0700

    Did codewars challenge for the day; Started NASA API homework; Updated continued learning list for info sec

--- 13-resumeBuildingNapis/lab_26/objects-nasa/js/main.js
+++ 13-resumeBuildingNapis/lab_26/objects-nasa/js/main.js
@@ -1,1 +1,15 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+document.querySelector('button').addEventListener('click', getAPOD())
+
+function getAPOD(){
+  const nasaPictureOfDay = fetch('https://api.nasa.gov/planetary/apod?api_key=A5wFDj5rJ7ZrafwFQ6k5Lxa7jyrDrTrfVWzLm8Ur') // query with api key
+  .then(res => res.json()) // grabs data in json format
+  .then(data => {
+    console.log(data);
+     document.querySelector('img').src = data.url; // places image on page
+    document.querySelector('.description').textContent = data.explanation;
+  })
+  .catch(err => {console.log(`error ${err} occured`)
+  });
+}
+
