a2a325894be040800ab0750e20da368408be70f1
Al.exe <alEXE-tech@protonmail.com>
2022-05-12 16:21:51 -0700

    Did my codewars for the day

---
+++ 00-exercises/codewars/8kyu/js/boolToString.js
@@ -0,0 +1,5 @@
+// Implement a function which convert the given boolean value into its string representation.
+//
+// Note: Only valid inputs will be given.
+
+booleanToString = b => b ? 'true' : 'false';
