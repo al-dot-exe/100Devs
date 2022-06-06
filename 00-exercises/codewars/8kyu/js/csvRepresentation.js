fedeed38554191301aa7a1fe00796026e02a2866
Al.exe <alEXE-tech@protonmail.com>
2022-06-06 11:25:50 -0700

    codewars challenge for the day

---
+++ 00-exercises/codewars/8kyu/js/csvRepresentation.js
@@ -0,0 +1,16 @@
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+// Array's length > 2.
+
+// input:
+//    [[ 0, 1, 2, 3, 4 ],
+//     [ 10,11,12,13,14 ],
+//     [ 20,21,22,23,24 ],
+//     [ 30,31,32,33,34 ]] 
+//     
+// output:
+//      '0,1,2,3,4\n'
+//     +'10,11,12,13,14\n'
+//     +'20,21,22,23,24\n'
+//     +'30,31,32,33,34'
+
+const toCsvText = row => row.join('\n');
