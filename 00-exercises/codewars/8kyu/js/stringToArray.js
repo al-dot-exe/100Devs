7884bf2afa260409531e97af36b70831cfdce7ae
Al.exe <alEXE-tech@protonmail.com>
2022-06-07 09:43:18 -0700

    Codewars challenge for the day

---
+++ 00-exercises/codewars/8kyu/js/stringToArray.js
@@ -0,0 +1,9 @@
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+//
+// "Robin Singh" ==> ["Robin", "Singh"]
+//
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+const stringToArray = str => str.split(' ');
