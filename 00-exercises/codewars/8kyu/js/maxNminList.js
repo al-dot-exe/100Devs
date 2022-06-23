90a3228ef5d1d4752e9a2c51edb2352ddc8f85ed
Al.exe <alEXE-tech@protonmail.com>
2022-06-23 09:07:38 -0700

    codewars

---
+++ 00-exercises/codewars/8kyu/js/maxNminList.js
@@ -0,0 +1,16 @@
+// Your task is to make two functions ( max and min, or maximum and minimum, etc., 
+// depending on the language ) that receive a list of integers as input, 
+// and return the largest and lowest number in that list, respectively.
+// Examples (Input -> Output)
+//
+// * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+// * [5]                             -> min = 5, max = 5
+//
+// Notes
+//
+//     You may consider that there will not be any empty arrays/vectors.
+
+var min = list => Math.min(...list);
+var max = list => Math.max(...list);
