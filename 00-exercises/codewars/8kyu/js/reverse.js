057e82b7e4d33f059903c7d59149789a15a2ce7e
Al.exe <alEXE-tech@protonmail.com>
2022-07-03 13:51:11 -0700

    Codewars for the day

---
+++ 00-exercises/codewars/8kyu/js/reverse.js
@@ -0,0 +1,7 @@
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+// Examples (Input -> Output)
+//
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+const reverseList = list => list.reverse();
