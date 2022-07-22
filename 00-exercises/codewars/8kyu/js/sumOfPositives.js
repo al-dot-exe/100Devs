735dd8ac6a27d3087672ea50bd3ef0c52378f94a
Al.exe <alEXE-tech@protonmail.com>
2022-07-22 09:40:39 -0700

    Daily Codewars challenge

---
+++ 00-exercises/codewars/8kyu/js/sumOfPositives.js
@@ -0,0 +1,7 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+//
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+//
+// Note: if there is nothing to sum, the sum is default to 0.
+
+const positiveSum = arr => arr.filter(n => n > 0).reduce((acc, num) => acc + num, 0);
